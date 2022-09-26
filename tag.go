0f853fd9c79673b27dd24d5e18452b28890b1102
Mya <j.mya.pitz@gmail.com>
2022-09-26 09:49:49 -0500

    tags: follow semantics from zap library

--- tag.go
+++ tag.go
@@ -9,45 +9,44 @@ import (
 
 type Tag = *pb.Tag
 
-func TagString(key string, val string) Tag {
+func String(key string, val string) Tag {
 	return &pb.Tag{
 		Key:   key,
 		Value: &pb.Tag_String_{String_: val},
 	}
 }
 
-func TagBytes(key string, val []byte) Tag {
+func Bytes(key string, val []byte) Tag {
 	return &pb.Tag{
 		Key:   key,
 		Value: &pb.Tag_Bytes{Bytes: val},
 	}
 }
 
-func TagInt64(key string, val int64) Tag {
+func Int64(key string, val int64) Tag {
 	return &pb.Tag{
 		Key:   key,
 		Value: &pb.Tag_Int64{Int64: val},
 	}
 }
 
-func TagFloat64(key string, val float64) Tag {
+func Float64(key string, val float64) Tag {
 	return &pb.Tag{
 		Key:   key,
 		Value: &pb.Tag_Double{Double: val},
 	}
 }
 
-func TagBool(key string, val bool) Tag {
+func Bool(key string, val bool) Tag {
 	return &pb.Tag{
 		Key:   key,
 		Value: &pb.Tag_Bool{Bool: val},
 	}
 }
 
-func TagDuration(key string, val time.Duration) Tag {
+func Duration(key string, val time.Duration) Tag {
 	return &pb.Tag{
 		Key:   key,
 		Value: &pb.Tag_Duration{Duration: durationpb.New(val)},
 	}
 }
-
