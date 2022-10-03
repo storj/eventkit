c4458db8ac4a0a5edd4056da39d0393285e28cb5
Egon Elbre <egonelbre@gmail.com>
2022-10-03 16:41:10 +0300

    pb: skip unnecessary checks

--- pb/utils.go
+++ pb/utils.go
@@ -6,11 +6,11 @@ import (
 
 func AsTimestamp(t time.Time) *Timestamp {
 	return &Timestamp{
-		Seconds: int64(t.Unix()),
+		Seconds: t.Unix(),
 		Nanos:   int32(t.Nanosecond()),
 	}
 }
 
 func (t *Timestamp) AsTime() time.Time {
-	return time.Unix(int64(t.GetSeconds()), int64(t.GetNanos())).UTC()
+	return time.Unix(t.GetSeconds(), int64(t.GetNanos())).UTC()
 }
