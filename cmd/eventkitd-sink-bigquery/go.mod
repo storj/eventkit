fc17df1830bb32e88bab94a627136dfa5211cec0
Márton Elek <elek@apache.org>
2022-11-08 10:56:28 +0100

    eventkitd-sink-bigquery: support creating schema on-the-fly

--- cmd/eventkitd-sink-bigquery/go.mod
+++ cmd/eventkitd-sink-bigquery/go.mod
@@ -8,19 +8,21 @@ require (
 	cloud.google.com/go/bigquery v1.42.0
 	github.com/jtolio/eventkit v0.0.0-20220927171925-c181046ab8ab
 	go.uber.org/zap v1.23.0
-	golang.org/x/sync v0.0.0-20220923202941-7f9b1623fab7
+	golang.org/x/sync v0.0.0-20220929204114-8fcdb60fdcc0
 )
 
 require (
 	cloud.google.com/go v0.104.0 // indirect
 	cloud.google.com/go/compute v1.10.0 // indirect
 	cloud.google.com/go/iam v0.4.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.5.1 // indirect
+	github.com/jtolio/eventkit/eventkitd v0.0.0-20221107155359-4ea4ead7a706 // indirect
 	github.com/klauspost/compress v1.15.11 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
--- cmd/eventkitd-sink-bigquery/main.go
+++ cmd/eventkitd-sink-bigquery/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"context"
 	"flag"
+	"google.golang.org/api/googleapi"
 	"net"
 	"os"
 	"os/signal"
-	"path"
+	"regexp"
 	"runtime"
+	"strings"
+	"sync"
 	"syscall"
 	"time"
 
@@ -53,7 +56,7 @@ func (r *Record) Save() (map[string]bigquery.Value, string, error) {
 	fields["correction"] = r.Correction
 
 	for _, tag := range r.Tags {
-		field := "tag_" + tag.Key
+		field := tagFieldName(tag.Key)
 
 		switch v := tag.Value.(type) {
 		case *pb.Tag_Bool:
@@ -76,7 +79,9 @@ func (r *Record) Save() (map[string]bigquery.Value, string, error) {
 
 // BigQuerySink provides an abstraction for processing events in a transport agnostic way.
 type BigQuerySink struct {
-	dataset *bigquery.Dataset
+	dataset          *bigquery.Dataset
+	tables           map[string]bigquery.TableMetadata
+	schemeChangeLock sync.Locker
 }
 
 // Receive is called when the server receive an event to process.
@@ -88,7 +93,7 @@ func (b *BigQuerySink) Receive(ctx context.Context, packet *Packet) error {
 		eventTime := correctedStart.Add(time.Duration(event.TimestampOffsetNs) * time.Nanosecond)
 		correction := correctedStart.Sub(packet.Packet.StartTimestamp.AsTime())
 
-		k := path.Join(event.Scope...) + "." + event.Name
+		k := tableName(event)
 
 		records[k] = append(records[k], &Record{
 			Application: Application{
@@ -106,7 +111,25 @@ func (b *BigQuerySink) Receive(ctx context.Context, packet *Packet) error {
 		})
 	}
 
+	var err error
 	for table, events := range records {
+		tableMetadata, found := b.tables[table]
+		if !found {
+			tableMetadata, err = b.createOrLoadTableScheme(ctx, table)
+		}
+		if err != nil {
+			return err
+		}
+
+		if len(events) > 0 {
+			if isTagMissing(tableMetadata.Schema, events[0].Tags) {
+				err = b.UpdateFields(ctx, tableMetadata, events[0].Tags)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
 		err := b.dataset.Table(table).Inserter().Put(ctx, events)
 		if err != nil {
 			return err
@@ -115,6 +138,144 @@ func (b *BigQuerySink) Receive(ctx context.Context, packet *Packet) error {
 
 	return nil
 }
+
+var nonSafeTableNameCharacters = regexp.MustCompile(`[^a-zA-Z0-9]+`)
+var multiUnderscore = regexp.MustCompile(`_{2,}`)
+
+func tableName(event *pb.Event) string {
+	var res []string
+	for _, scope := range event.Scope {
+		res = append(res, nonSafeTableNameCharacters.ReplaceAllString(scope, "_"))
+	}
+	res = append(res, nonSafeTableNameCharacters.ReplaceAllString(event.Name, "_"))
+
+	name := strings.Join(res, "_")
+	all := multiUnderscore.ReplaceAllString(name, "_")
+	all = strings.Trim(all, "_")
+	return all
+}
+
+func isTagMissing(schema bigquery.Schema, tags []*pb.Tag) bool {
+tagloop:
+	for _, tag := range tags {
+		for _, field := range schema {
+			if field.Name == tagFieldName(tag.Key) {
+				continue tagloop
+			}
+		}
+		return true
+	}
+	return false
+}
+
+func (b *BigQuerySink) createOrLoadTableScheme(ctx context.Context, table string) (bigquery.TableMetadata, error) {
+	b.schemeChangeLock.Lock()
+	defer b.schemeChangeLock.Unlock()
+	tableMetadata, found := b.tables[table]
+	if found {
+		return tableMetadata, nil
+	}
+	meta, err := b.dataset.Table(table).Metadata(ctx)
+	switch e := err.(type) {
+	case *googleapi.Error:
+		if e.Code == 404 {
+			err = b.dataset.Table(table).Create(ctx, &bigquery.TableMetadata{
+				Name: table,
+				Schema: bigquery.Schema{
+					{
+						Name: "application_name",
+						Type: bigquery.StringFieldType,
+					},
+					{
+						Name: "application_version",
+						Type: bigquery.StringFieldType,
+					},
+					{
+						Name: "source_instance",
+						Type: bigquery.StringFieldType,
+					},
+					{
+						Name: "source_ip",
+						Type: bigquery.StringFieldType,
+					},
+					{
+						Name: "received_at",
+						Type: bigquery.TimestampFieldType,
+					},
+					{
+						Name: "timestamp",
+						Type: bigquery.TimestampFieldType,
+					},
+					{
+						Name: "correction",
+						Type: bigquery.IntegerFieldType,
+					},
+				},
+			})
+			if err != nil {
+				return tableMetadata, err
+			}
+			meta, err = b.dataset.Table(table).Metadata(ctx)
+			if err != nil {
+				return tableMetadata, err
+			}
+
+		}
+	}
+	b.tables[table] = *meta
+	return *meta, err
+}
+
+func (b *BigQuerySink) UpdateFields(ctx context.Context, metadata bigquery.TableMetadata, tags []*pb.Tag) error {
+	b.schemeChangeLock.Lock()
+	defer b.schemeChangeLock.Unlock()
+	schema := metadata.Schema
+tagloop:
+	for _, tag := range tags {
+		for _, field := range metadata.Schema {
+			if field.Name == tagFieldName(tag.Key) {
+				continue tagloop
+			}
+		}
+
+		// missing field
+		f := &bigquery.FieldSchema{
+			Name: tagFieldName(tag.Key),
+		}
+		switch tag.Value.(type) {
+		case *pb.Tag_Bool:
+			f.Type = bigquery.BooleanFieldType
+		case *pb.Tag_Bytes:
+			f.Type = bigquery.BytesFieldType
+		case *pb.Tag_Double:
+			f.Type = bigquery.FloatFieldType
+		case *pb.Tag_DurationNs:
+			f.Type = bigquery.IntegerFieldType
+		case *pb.Tag_Int64:
+			f.Type = bigquery.IntegerFieldType
+		case *pb.Tag_String_:
+			f.Type = bigquery.StringFieldType
+		}
+		schema = append(schema, f)
+	}
+	md, err := b.dataset.Table(metadata.Name).Update(ctx, bigquery.TableMetadataToUpdate{
+		Schema: schema,
+	}, metadata.ETag)
+	if err != nil {
+		return err
+	}
+	b.tables[metadata.Name] = *md
+	return nil
+}
+
+func tagFieldName(key string) string {
+	field := "tag_" + key
+	field = strings.ReplaceAll(field, "/", "_")
+	field = strings.ReplaceAll(field, "-", "_")
+	return field
+}
+
+var _ bigquery.ValueSaver = &Record{}
 
 type Config struct {
 	Address *string
@@ -163,7 +324,11 @@ func main() {
 		return
 	}
 
-	sink := &BigQuerySink{dataset: client.Dataset(*cfg.Google.BigQuery.Dataset)}
+	sink := &BigQuerySink{
+		dataset:          client.Dataset(*cfg.Google.BigQuery.Dataset),
+		tables:           map[string]bigquery.TableMetadata{},
+		schemeChangeLock: &sync.Mutex{},
+	}
 
 	group, ctx := errgroup.WithContext(ctx)
 	workQueue := make(chan *Packet, *cfg.Workers*2)
